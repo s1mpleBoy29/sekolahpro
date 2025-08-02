8ff0338cbc9d23ab5f12e2180aeeb57d29383e6a
dandelionsforyou <imanaka1234@gmail.com>
2025-08-02 09:23:22 +0700

    update-pembayaran

--- lib/presentation/pembayaran/bayarsatu.dart
+++ lib/presentation/pembayaran/bayarsatu.dart
@@ -2,13 +2,50 @@ import 'package:flutter/material.dart';
 import 'package:guardian_app/core/app_export.dart';
 import 'package:guardian_app/presentation/pembayaran/widgets/instruction_card.dart';
 import 'package:guardian_app/presentation/pembayaran/widgets/payment_steps.dart';
+import 'package:guardian_app/presentation/pembayaran/widgets/due_card_small.dart';
+// import 'package:guardian_app/presentation/pembayaran/widgets/bottom_bar.dart';
 
 class BayarSatuScreen extends StatefulWidget {
+  const BayarSatuScreen({super.key});
   @override
   _BayarSatuState createState() => _BayarSatuState();
 }
 
 class _BayarSatuState extends State<BayarSatuScreen> {
+  final List<Map<String, dynamic>> _dueItems = [
+    {
+      "id": 1,
+      "isOverdue": true,
+      "harga": "Rp 300.000",
+      "deskripsi": "Uang Sekolah Chandra Bulan Juli\nTahun Ajaran 2025 / 2026",
+    },
+    {
+      "id": 2,
+      "isOverdue": false,
+      "harga": "Rp 1.200.000",
+      "deskripsi": "Uang Buku & Seragam\nTahun Ajaran 2025 / 2026",
+    },
+    {
+      "id": 3,
+      "isOverdue": false,
+      "harga": "Rp 300.000",
+      "deskripsi":
+          "Uang Sekolah Chandra Bulan Agustus\nTahun Ajaran 2025 / 2026",
+    }
+  ];
+
+  final Set<int> _selectedCardIndices = {0};
+
+  void _toggleCardSelection(int index) {
+    setState(() {
+      if (_selectedCardIndices.contains(index)) {
+        _selectedCardIndices.remove(index);
+      } else {
+        _selectedCardIndices.add(index);
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,21 +59,33 @@ class _BayarSatuState extends State<BayarSatuScreen> {
           ),
         ),
       ),
-      body: const Column(
+      body: Column(
         children: [
           Expanded(
             child: SingleChildScrollView(
-              padding: EdgeInsets.all(16.0),
+              padding: const EdgeInsets.all(16.0),
               child: Column(
                 children: [
-                  PaymentSteps(stepsekarang: 0),
-                  SizedBox(
-                    height: 12.0,
-                  ),
-                  InstructionCard(
+                  const PaymentSteps(stepsekarang: 0),
+                  const SizedBox(height: 12.0),
+                  const InstructionCard(
                     number: '1',
-                    teksInstruksi:
-                        'Pembayaran kewajiban siswa dapat dilakukan melalui Transfer Bank ke salah satu rekening berikut',
+                    teksInstruksi: 'Pilih kewajiban yang harus dibayar.',
+                  ),
+                  ListView.builder(
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    itemCount: _dueItems.length,
+                    itemBuilder: (context, index) {
+                      final item = _dueItems[index];
+                      return DueCard(
+                        isOverdue: item['isOverdue'],
+                        harga: item['harga'],
+                        deskripsi: item['deskripsi'],
+                        isSelected: _selectedCardIndices.contains(index),
+                        onTap: () => _toggleCardSelection(index),
+                      );
+                    },
                   ),
                 ],
               ),
